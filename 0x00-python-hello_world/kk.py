b0e0a61b0ee9f85b0eba0d2b40ebd648d435e47a
mulerrrr4 <mulerrrr4@gmail.com>
2023-04-03 14:14:42 -0700

    f

---
+++ 0x00-python-hello_world/kk.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def mul(a,b):
+	try:
+		if a > 5:
+			print("trr")
+	raise Exception("sorry")
+		
+mul(t,3)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x06. Python - Classes and Objects
+0x08. Python - More Classes and Objects
