9a4c17f35bb7da3836db92fd4493d5ef0e97eae7
mulerrrr4 <mulerrrr4@gmail.com>
2023-04-11 13:47:29 -0700

    y

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-"""This module contains a function called number_of_lines"""
 
 
-def number_of_lines(filename=""):
-    """number_of_lines function that find the number if lines in a file
-    Args:
-        filename (str, optional): file name string. Defaults to "".
-    Returns:
-        [int]: number of lines in the file
-    """
-    with open("{}".format(filename), encoding='utf-8') as a_file:
-        lines = 0
-        for a_line in a_file:
-            lines += 1
-        return lines
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
 
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
-"""This module contain a function called read_lines"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """read_lines function that read nb_lines of a file
-    Args:
-        filename (str, optional): file name. Defaults to "".
-        nb_lines (int, optional): lines to read from the file. Defaults to 0.
-    """
-    num_lines = 0
-    with open(filename, encoding='UTF-8') as a_file:
-        for _ in a_file:
-            num_lines += 1
-    with open(filename, encoding='UTF-8') as a_file:
-        if nb_lines <= 0 or nb_lines >= num_lines:
-            for i in a_file:
-                print(i, end="")
-        else:
-            j = 0
-            while j < nb_lines:
-                print(a_file.readline(), end="")
-                j += 1
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
 
